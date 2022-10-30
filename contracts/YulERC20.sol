d05cac2066ac11bd0288c6ac720bb00782ee4924
kassandra <0xdada@protonmail.com>
2022-10-30 16:20:05 -0400

    remove interface from src

--- contracts/YulERC20.sol
+++ contracts/YulERC20.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-contract YulERC20 is IERC20 {
+contract YulERC20 {
     error InsufficientBalance();
 
     error InsufficientAllowance();
